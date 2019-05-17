3eca1ae98a682bd15664bc1e59c841c8d325de5e
cmohri <clara.mohri@gmail.com>
2019-05-17 10:53:49 -0400

    Update minutes

--- static/source.js
+++ static/source.js
@@ -116,12 +116,6 @@ function mins_into_left(){
     return (a, b);
 }
 	
-                                 
-
-
-
-
-
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;}
