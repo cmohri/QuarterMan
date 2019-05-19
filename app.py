c3ee509100ede23ae02e3aaba40d72164eaad57b
Damian Wasilewicz <dwasilewicz@stuy.edu>
2019-05-19 17:38:28 -0400

    Template Creation Working

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ app = Flask(__name__)
 app.secret_key = urandom(32)
 app.config.from_object(Config)
 
-class LoginForm(FlaskForm):
+class TemplateForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    numpds = PasswordField('Number of Periods', validators=[DataRequired()])
-    p1title  = PasswordField('Period 1 Title', validators=[DataRequired()])
-    p1time = PasswordField('Period 1 Time', validators=[DataRequired()])
-    p2title = PasswordField('Period 2 Title', validators=[DataRequired()])
-    p2time = PasswordField('Period 2 Time', validators=[DataRequired()])
-    p3title = PasswordField('Period 3 Title', validators=[DataRequired()])
-    p3time = PasswordField('Period 3 Time', validators=[DataRequired()])
+    numpds = StringField('Number of Periods', validators=[DataRequired()])
+    p1title  = StringField('Period 1 Title', validators=[DataRequired()])
+    p1time = StringField('Period 1 Time', validators=[DataRequired()])
+    p2title = StringField('Period 2 Title', validators=[DataRequired()])
+    p2time = StringField('Period 2 Time', validators=[DataRequired()])
+    p3title = StringField('Period 3 Title', validators=[DataRequired()])
+    p3time = StringField('Period 3 Time', validators=[DataRequired()])
     submit = SubmitField('Create Template')
 
 @app.route("/", methods = ['GET', 'POST'])
@@ -28,15 +28,16 @@ def home():
 
 @app.route('/maketemp', methods = ['GET', 'POST'])
 def maketemp():
-    form = LoginForm()
+    form = TemplateForm()
     if form.validate_on_submit():
-        return redirect('/')
+        return render_template('template.html', form = form)
     return render_template('maketemp.html', title='Create Template Schedule', form=form)
 
 @app.route("/template", methods = ['GET', 'POST'])
 def template():
-    return render_template('template.html')
-    
+    form = TemplateForm()
+    return render_template('template.html', form = form)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
