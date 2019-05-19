0d408306728260bbcfde1a872b221b3a69969ca0
Damian Wasilewicz <dwasilewicz@stuy.edu>
2019-05-19 17:23:23 -0400

    worked on template

--- app.py
+++ app.py
@@ -14,17 +14,29 @@ app.config.from_object(Config)
 class LoginForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     numpds = PasswordField('Number of Periods', validators=[DataRequired()])
+    p1title  = PasswordField('Period 1 Title', validators=[DataRequired()])
+    p1time = PasswordField('Period 1 Time', validators=[DataRequired()])
+    p2title = PasswordField('Period 2 Title', validators=[DataRequired()])
+    p2time = PasswordField('Period 2 Time', validators=[DataRequired()])
+    p3title = PasswordField('Period 3 Title', validators=[DataRequired()])
+    p3time = PasswordField('Period 3 Time', validators=[DataRequired()])
     submit = SubmitField('Create Template')
 
 @app.route("/", methods = ['GET', 'POST'])
 def home():
     return render_template('home.html', a = str(datetime.datetime.now())[11:19])
 
-@app.route('/login')
-def login():
+@app.route('/maketemp', methods = ['GET', 'POST'])
+def maketemp():
     form = LoginForm()
-    return render_template('login.html', title='Create Template Schedule', form=form)
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('maketemp.html', title='Create Template Schedule', form=form)
 
+@app.route("/template", methods = ['GET', 'POST'])
+def template():
+    return render_template('template.html')
+    
 if __name__ == '__main__':
     app.debug = True
     app.run()
