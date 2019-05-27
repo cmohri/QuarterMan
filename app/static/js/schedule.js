2213b5e2d4e35b67cd5859b3f88ccc6e2b18f291
Jared Asch <jaredasch1@gmail.com>
2019-05-26 21:41:02 -0400

    redesign schedule creation

---
+++ app/static/js/schedule.js
@@ -0,0 +1,40 @@
+let inputTemplateGenerator = (slot_id) =>
+    `<div class = "row">
+        <div class = "input-group col-lg-11 schedule-slot">
+            <input name = "${slot_id}-name" class = "col-lg-8 col-md-6 form-control" placeholder="Schedule Slot Name"/>
+            <span class = "input-group-append">Start Time: </span>
+            <input name = "${slot_id}-start" class = "col-lg-2 col-md-3 form-control" placeholder="24 hour time"/>
+            <span class = "input-group-append">End Time: </span>
+            <input name = "${slot_id}-end" class = "col-lg-2 col-md-3 form-control" placeholder="24 hour time"/>
+        </div>
+        <div class = "col-lg-1">
+            <svg class = "remove-slot" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+                <!-- Created with Method Draw - http://github.com/duopixel/Method-Draw/ -->
+                <ellipse ry="10" rx="10" id="svg_1" cy="10" cx="10" stroke-width="0" stroke="#000" fill="#dc3545" fill-opacity="0.85"/>
+                <rect id="svg_2" height="2.5" width="10" y="8.75" x="5" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#ffffff"/>
+            </svg>
+        </div>
+        <hr class = "input-divider">
+    </div>`;
+
+function addSlot(){
+    let id = document.getElementsByClassName("schedule-slot").length;
+    let new_slot = inputTemplateGenerator(id);
+    let wrapper = document.createElement("div");
+    wrapper.innerHTML = new_slot;
+    document.getElementById("schedule-form").appendChild(wrapper.firstChild);
+    new_slot = document.getElementById("schedule-form").lastChild;
+    new_slot.getElementsByClassName("remove-slot")[0].addEventListener("click", function (e) {
+        new_slot.parentNode.removeChild(new_slot);
+    });
+}
+
+document.getElementById("add-slot").addEventListener("click", function () {
+    addSlot();
+});
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    for(let i = 0; i < 5; i++){
+        addSlot();
+    }
+});
