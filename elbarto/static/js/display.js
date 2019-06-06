2efb54b0e2729f910bc40db711142427efd0cbf5
cmohri <clara.mohri@gmail.com>
2019-06-06 18:45:02 -0400

    Add am/pm

--- elbarto/static/js/display.js
+++ elbarto/static/js/display.js
@@ -2,7 +2,14 @@ function timeString(time){
     let hours = Math.floor(time / 3600);
     let minutes = Math.floor((time - hours * 3600) / 60);
     let seconds = time % 60;
-    return hours % 12 + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2);
+    if (hours >= 12){
+	post = "PM";
+    }
+    else {
+	post = "AM";
+    }
+    
+    return hours % 12 + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2) + " " + post;
 }
 
 function timeLoop(time, schedule){
