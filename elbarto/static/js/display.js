6510fe65ff736a52eb77050f99e0b8dd24b9023b
cmohri <clara.mohri@gmail.com>
2019-06-06 00:40:22 -0400

    add highlighting and minutes

--- elbarto/static/js/display.js
+++ elbarto/static/js/display.js
@@ -7,21 +7,61 @@ function timeString(time){
 
 function timeLoop(time, schedule){
     // Update the clock
+    console.log(time);
     timeText = document.getElementById("time");
     let hours = Math.floor(time / 3600);
     let minutes = Math.floor((time - hours * 3600) / 60);
     let seconds = time % 60;
     timeText.innerHTML = timeString(time);
-    // Highlight period
     let period = 0;
-    while(period < schedule.length && schedule[period].start < time){
-        period += 1;
+    while (period < schedule.length && schedule[period].start < time){
+	period += 1;
     }
-    // Show minutes into/left
+    //console.log(period - 1);
+    if (period != 0){
+	period -= 1;
+    }
+
+    if (schedule[period].start > time){
+	var into ="none";
+	var left = "none";
+
+    } else if (schedule[period].end > time ){
+
+	// Show minutes into/left
+	var into = ( Math.floor((time - schedule[period].start) / 60));
+	var left = ( Math.floor((schedule[period].end - time) / 60));
+	// Highlight period
+	document.getElementById("slot-" + period.toString()).style.color = "red";
+	
+    } else { // if end time is also less than time
+
+	if (period + 1 < schedule.length){ // if not at end of schedule
+	    
+	    var into = ( Math.floor ( (time - schedule[period].end)  / 60));
+	    var left = ( Math.floor( ((schedule[period + 1].start - time) / 60)));
+	    var n = period + 1;
+	    document.getElementById("slot-" + period.toString()).style.color = "red";
+	    document.getElementById("slot-" + n.toString()).style.color = "red";
+	}
+	else { // past end of schedule
+	    var into ="none";
+            var left = "none";
+	}
+	    
+    }
+
+    document.getElementById('minutes_into').innerHTML  = into;     
+    document.getElementById("minutes_left").innerHTML = left;      
+
+
+	
+    
     setTimeout(timeLoop, 1000, time + 1, schedule);
 }
 
 function initialize(schedule){
-    let t = new Date()
+    console.log(schedule);
+    let t = new Date();
     timeLoop(t.getHours() * 3600 + t.getMinutes() * 60 + t.getSeconds(), schedule);
 }
